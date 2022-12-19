aa04028ee34ee509cd79c339f10d582517a9669c
Alina <lina.millou@yandex.ru>
2022-12-19 15:30:24 +0300

    add Example_02 and gitignore

--- Example_01_HelloConsole/Program.cs
+++ Example_01_HelloConsole/Program.cs
@@ -64,14 +64,14 @@
 // -3 -> нет
 // 7 -> нет
 
-Console.WriteLine("Введите число:");
-            int num = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число:");
+//             int num = Convert.ToInt32(Console.ReadLine());
 
-            if (num % 2 == 1)
-            {
-                Console.WriteLine("Число " + num + " является: НЕЧЁТНЫМ");
-            }
-            else
-            {
-                Console.WriteLine("Число " + num + " является: ЧЁТНЫМ");
-            }
+//             if (num % 2 == 1)
+//             {
+//                 Console.WriteLine("Число " + num + " является: НЕЧЁТНЫМ");
+//             }
+//             else
+//             {
+//                 Console.WriteLine("Число " + num + " является: ЧЁТНЫМ");
+//             }
---
+++ Example_02/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите ваше имя ");
+string username = Console.ReadLine();
+Console.WriteLine("Привет, ");
+Console.WriteLine(username);
