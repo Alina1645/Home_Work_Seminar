3367548d9120c58f1f84c29d62bfbdb8a6797f91
Alina <lina.millou@yandex.ru>
2022-12-19 14:21:01 +0300

    Создали папку Example_01

---
+++ Example_01_HelloConsole/Program.cs
@@ -0,0 +1,77 @@
+﻿// Console.WriteLine(" Input Number ");
+// int x = Convert.ToInt32(Console.ReadLine());
+// int y = x * x;
+// Console.WriteLine($"Quad of {x} -> {y}");
+
+
+// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+// Console.WriteLine("Введите первое число:");
+
+//            int a = Convert.ToInt32(Console.ReadLine());
+
+//            Console.WriteLine("Введите второе число:");
+
+//            int b = Convert.ToInt32(Console.ReadLine());
+
+//            Console.WriteLine(a > b ? "Первое число больше второго" : "Второе число больше первого");
+
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+// Console.WriteLine("Введите первое число: ");
+// int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите второе число: ");
+// int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите третье число: ");
+// int thirdNumber = Convert.ToInt32(Console.ReadLine());
+
+// if (firstNumber > secondNumber)
+// {
+//     if (firstNumber > thirdNumber)
+//     {
+//         Console.WriteLine("Максимальное число: " + firstNumber);
+//     }
+//     else
+//     {
+//         Console.WriteLine("Максимальное число: " + thirdNumber);
+//     }
+// }
+
+// else if (secondNumber > thirdNumber)
+// {
+//     Console.WriteLine("Максимальное число: " + secondNumber);
+// }
+// else
+// {
+//     Console.WriteLine("Максимальное число: " + thirdNumber);
+// }
+
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт,
+//  является ли число чётным (делится ли оно на два без остатка).
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.WriteLine("Введите число:");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            if (num % 2 == 1)
+            {
+                Console.WriteLine("Число " + num + " является: НЕЧЁТНЫМ");
+            }
+            else
+            {
+                Console.WriteLine("Число " + num + " является: ЧЁТНЫМ");
+            }
