1ae583eeb47f69247827b8726272660dba1adf4d
Alina <lina.millou@yandex.ru>
2022-12-20 14:46:03 +0300

    Домашнее задание 1 семинар

--- Example_01/Program.cs
+++ Example_01/Program.cs
@@ -4,6 +4,8 @@
 // Console.WriteLine($"Quad of {x} -> {y}");
 
 
+                //Домашняя работа 1 семинар:
+
 // Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
 // a = 5; b = 7 -> max = 7
@@ -19,6 +21,7 @@
 //            int b = Convert.ToInt32(Console.ReadLine());
 
 //            Console.WriteLine(a > b ? "Первое число больше второго" : "Второе число больше первого");
+
 
 
 // Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
@@ -57,6 +60,8 @@
 //     Console.WriteLine("Максимальное число: " + thirdNumber);
 // }
 
+
+
 // Задача 6: Напишите программу, которая на вход принимает число и выдаёт,
 //  является ли число чётным (делится ли оно на два без остатка).
 
@@ -64,14 +69,37 @@
 // -3 -> нет
 // 7 -> нет
 
-// Console.WriteLine("Введите число:");
-//             int num = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
 
-//             if (num % 2 == 1)
-//             {
-//                 Console.WriteLine("Число " + num + " является: НЕЧЁТНЫМ");
-//             }
-//             else
-//             {
-//                 Console.WriteLine("Число " + num + " является: ЧЁТНЫМ");
-//             }
+// int remDiv = number % 2;
+
+// if(remDiv == 0)
+// {
+//     Console.WriteLine("Да");
+// }
+// else
+// {
+//     Console.WriteLine("Нет");
+// }
+
+
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+
+// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// int evenNumber = 2;
+
+// if(number > 1)
+// {
+//     while(evenNumber <= number)
+//     {
+//         Console.Write(evenNumber + " ");
+//         evenNumber = evenNumber + 2;
+//     }
+// }
