b9a4d72e6dd7e84c2bc5005ac676bd0e78588c15
Alina <lina.millou@yandex.ru>
2022-12-27 18:04:19 +0300

    Домашнее задание  Example_06

--- Example_06/Program.cs
+++ Example_06/Program.cs
@@ -4,85 +4,86 @@
 
 // 1, -7, 567, 89, 223-> 3
 
+Console.WriteLine("Введите числа через запятую: ");
+string? input = Console.ReadLine();
+int[] numbers = ParseStringToArray(input);
+PrintArray(numbers);
 
-// Console.Write("Введите числа через запятую: ");
-// int[] numbers = StringToNum(Console.ReadLine());
-// PrintArray(numbers);
-// int sum = 0;
-// for (int i = 0; i < numbers.Length; i++)
-// {
-//     if (numbers[i] > 0)
-//     {
-//         sum++;
-//     }
-// }
-// Console.WriteLine();
-// Console.WriteLine($"количество значений больше 0 = {sum}");
+ 
+int Comparison(int[] Numbers)
+{
+  int count = 0;
+  for (int i = 0; i < numbers.Length; i++)
+  {
+    if(Numbers[i] > 0 ) count += 1; 
+  }
+  return count;
+}
+ 
+Console.WriteLine($"Введено чисел больше нуля >> {Comparison(numbers)} ");
 
+int[] ParseStringToArray(string input)
+{
+    int countNumbers = GetCountNumbersInString(input);
+    int[] numbers = new int[countNumbers];
+    int indexNumber = 0;
+    string substring = string.Empty;
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i] != ',')
+        {
+            substring += input[i];
+        }
+        else
+        {
+            numbers[indexNumber] = Convert.ToInt32(substring);
+            indexNumber++;
+            substring = string.Empty;
+        }
+    }
+    if(input[input.Length - 1] != ',')
+        numbers[indexNumber] = Convert.ToInt32(substring);
+    return numbers;
+}
+int GetCountNumbersInString(string input) 
+{
+    int count;
+    if(input[input.Length - 1] == ',')
+        count = 0;
+    else
+        count = 1;
 
-// int[] StringToNum(string input)
-// {
-//     int count = 1;
-//     for (int i = 0; i < input.Length; i++)
-//     {
-//         if (input[i] == ',')
-//         {
-//             count++;
-//         }
-//     }
+    for(int i = 0; i < input.Length; i++)
+    {
+        if(input[i] == ',')
+            count++;
+    }
+    return count;
+}
 
-//     int[] numbers = new int [count];
-//     int index = 0;
-
-//     for (int i = 0; i < input.Length; i++)
-//     {
-//         string temp = "";
-
-//         while (input [i] != ',')
-//         {
-//         if(i != input.Length - 1)
-//         {
-//             temp += input [i].ToString();
-//             i++;
-//         }
-//         else
-//         {
-//             temp += input [i].ToString();
-//             break;
-//         }
-//         }
-//         numbers[index] = Convert.ToInt32(temp);
-//         index++;
-//     }
-//     return numbers;
-// }
-
-
-// void PrintArray(int[] array)
-// {
-//     Console.Write("[ ");
-//     for (int i = 0; i < array.Length; i++)
-//     {
-//         Console.Write(array[i] + " ");
-//     }
-//     Console.Write("]");
-// }
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
 
 
 // Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
-// Console.WriteLine("введите значение b1");
-// double b1 = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("введите число k1");
-// double k1 = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("введите значение b2");
-// double b2 = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("введите число k2");
-// double k2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите значение b1");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число k1");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите значение b2");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число k2");
+double k2 = Convert.ToInt32(Console.ReadLine());
 
-// double x = (-b2 + b1)/(-k1 + k2);
-// double y = k2 * x + b2;
+double x = (-b2 + b1)/(-k1 + k2);
+double y = k2 * x + b2;
 
-// Console.WriteLine($"две прямые пересекутся в точке с координатами X: {x}, Y: {y}");
+Console.WriteLine($"две прямые пересекутся в точке с координатами X: {x}, Y: {y}");
